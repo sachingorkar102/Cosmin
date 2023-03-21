7414c13a54cacbeec2592799db095458ea6f58c9
sachingorkar102 <sachingorkarcj72019@gmail.com>
2023-03-21 20:03:49 +0530

    ItemsAdder support

---
+++ core/src/main/java/com/github/sachin/cosmin/integration/ItemsAddersAPI.java
@@ -0,0 +1,21 @@
+package com.github.sachin.cosmin.integration;
+
+import com.github.sachin.cosmin.Cosmin;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemsAddersAPI {
+
+    public static final boolean isEnabled;
+
+    static {
+        isEnabled = Cosmin.getInstance().getServer().getPluginManager().isPluginEnabled("ItemsAdder");
+    }
+
+    public static ItemStack getItem(String name){
+        return dev.lone.itemsadder.api.CustomStack.getInstance(name).getItemStack();
+    }
+
+    public static boolean isInRegistry(String name){
+        return dev.lone.itemsadder.api.CustomStack.isInRegistry(name);
+    }
+}
